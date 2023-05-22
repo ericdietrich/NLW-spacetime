990d1fe2d77641ba893a525f4d2f2cef198d6d8b
Eric Dietrich <eric.dietrich@alpes.one>
2023-05-21 21:56:43 -0300

    web initial config

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,10 +1,5 @@
-import Button from "./components/Button";
-
 export default function Home() {
-  return ( <div>
-
-    <Button  title="Eric"/>
-    <Button  title="Rafaela"/>
+  return <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+    <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
   </div>
-    )
 }
