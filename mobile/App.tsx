e8b6acecd35bce021693d1f2a2f19db565bc8b6a
Eric Dietrich <eric.dietrich@alpes.one>
2023-06-14 07:31:51 -0300

    add svg library and created svg component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,8 +4,13 @@ import { ImageBackground } from 'react-native';
 import {useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree';
 import blurBg from './src/assets/bg-blur.png';
+import Stripes from './src/assets/stripes.svg';
+import { styled } from 'nativewind';
 
 export default function App() {
+  // recebe um componente não nativo do native para passar a ser estilizado pelo nativewind
+  const StyledStripes = styled(Stripes);
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular, 
     Roboto_700Bold,
@@ -21,6 +26,7 @@ export default function App() {
     <ImageBackground source={blurBg} imageStyle={{position: 'absolute', left:'-100%'}} className="flex-1 items-center relative bg-gray-900" >
       
       <StatusBar style="light" translucent />
+      <StyledStripes className='absolute left-2'  />
     </ImageBackground>
   );
 }
