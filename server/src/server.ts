abc056b6362721ed49c83867ee0c035eb54998fc
Eric Dietrich <eric.dietrich@alpes.one>
2023-06-30 08:44:00 -0300

    add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,12 @@
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true, // permite que qualquer front-end acesse as rotas
+})
 
 app.register(memoriesRoutes)
 
