a23720df83ec79e1fd673fafe95aa7cdd44f51bc
Eric Dietrich <eric.dietrich@alpes.one>
2023-07-18 08:25:47 -0300

    add auth route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -9,6 +11,7 @@ app.register(cors, {
 })
 
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
