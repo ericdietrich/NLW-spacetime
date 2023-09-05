d6605ef2b55a6f1ed9aa8efd7d2927a7a4026bc4
Eric Dietrich <eric.dietrich@alpes.one>
2023-09-05 08:29:16 -0300

    auth route

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -1,5 +1,15 @@
+import { api } from "@/lib/api";
 import { NextRequest } from "next/server";
 
 export async function GET (request : NextRequest){
-  console.log('oi')
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
+
+  const RegisterResponse = await api.post('/register', {
+    code
+  })
+
+  const {token} = RegisterResponse.data
+
+  console.log (token)
 }
